c2a0651b506f0650b547763070c753d3e834bd4b
Lavsexpert <lavrov-sergey@yandex.ru>
2019-01-28 20:55:56 +0300

    Научились обращаться к элементам из кода и ресурсов, подключать обработку событий, работать с бандлами

--- app/src/main/java/plus1/club/MainActivity.java
+++ app/src/main/java/plus1/club/MainActivity.java
@@ -1,13 +1,39 @@
 package plus1.club;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnClick;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String newString = getString(R.string.new_text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getColor(R.color.red);
+        }
+        ContextCompat.getColor(this, R.color.red);
+
+        mText = findViewById(R.id.tv_text);
+        mBtnClick = findViewById(R.id.btn_click);
+        mBtnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mText.setText(newString);
+            }
+        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("KEY", "SAD");
     }
 }
